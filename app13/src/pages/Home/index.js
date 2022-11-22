5549323422b87a8e89e25f02e930d1f203c39741
Kaique Ortolani Lusvarghi <99893722+KaiqueLusvarghi@users.noreply.github.com>
2022-11-22 10:11:51 -0300

    Add files via upload

---
+++ app13/src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+ 
+export default function Home() {
+ return (
+   <View >
+     <Text style={{color: '#00BFFF', fontSize: 15, margin: 15}}> 
+     Olá esse é meu perfil, Me chamo Kaique, tenho 21 anos. Clique nas proximas telas para saber mais sobre mim....</Text>
+   </View>
+  );
+}
